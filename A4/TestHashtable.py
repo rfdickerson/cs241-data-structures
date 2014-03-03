81017af9dc41e839a47a51ee025beb1bff93dff0
Quint Guvernator <quintus.public@gmail.com>
2014-03-03 17:48:30 -0500

    now tests large and small dictionaries
    - "large" dictionaries shouldn't collide too much
    - "small" dictionaries collide a lot as a rule

--- A4/TestHashtable.py
+++ A4/TestHashtable.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2
 
-from hashtable import Hashtable, LinkedList, hashFunction
+from hashtable import Hashtable, hashFunction
 import unittest
 import collections
 
@@ -15,39 +15,74 @@ class TestHashtable(unittest.TestCase):
             "LING" : "Tyler",
             "GERM" : "Washington",
         }
+        self.doubles = { n : n + n for n in range(193) }
+        self.testingFunction = lambda k, n: sum(map(ord, str(k))) % n
 
-    def testWithoutFunction(self):
-        testingFunction = lambda key, numBuckets: sum(map(ord, key)) % numBuckets
-        q = Hashtable(testingFunction, 1000)
+    def testSmallGetSetWithoutFunction(self):
+        q = Hashtable(self.testingFunction, 3)
         for key, value in self.buildings.iteritems():
             q[key] = value
         for key, expected in self.buildings.iteritems():
             observed = q[key]
             self.assertEquals(observed, expected, "small hashtable without your hash function: value changed after being added!\nkey:{}\nexpected value:{}\nobserved value:{}".format(key, value, q[key]))
 
-    def testWithFunction(self):
-        q = Hashtable(hashFunction, 1000)
+    def testLargeGetSetWithoutFunction(self):
+        q = Hashtable(self.testingFunction, 800)
+        for key, value in self.doubles.iteritems():
+            q[key] = value
+        for key, expected in self.doubles.iteritems():
+            observed = q[key]
+            self.assertEquals(observed, expected, "large hashtable without your hash function: value changed after being added!\nkey:{}\nexpected value:{}\nobserved value:{}".format(key, value, q[key]))
+
+    def testSmallGetSetWithFunction(self):
+        q = Hashtable(hashFunction, 3)
         for key, value in self.buildings.iteritems():
             q[key] = value
         for key, expected in self.buildings.iteritems():
             observed = q[key]
             self.assertEquals(observed, expected, "small hashtable with your hash function: value changed after being added! check __getitem__/__setitem__\nkey:{}\nexpected value:{}\nobserved value:{}".format(key, value, q[key]))
 
-    def testContains(self):
-        q = Hashtable(hashFunction, 1000)
+    def testLargeGetSetWithFunction(self):
+        q = Hashtable(hashFunction, 800)
+        for key, value in self.doubles.iteritems():
+            q[key] = value
+        for key, expected in self.doubles.iteritems():
+            observed = q[key]
+            self.assertEquals(observed, expected, "large hashtable with your hash function: value changed after being added! check __getitem__/__setitem__\nkey:{}\nexpected value:{}\nobserved value:{}".format(key, value, q[key]))
+
+    def testSmallContains(self):
+        q = Hashtable(hashFunction, 3)
         for key, value in self.buildings.iteritems():
             q[key] = value
         for key in self.buildings:
             self.assertIn(key, q, "membership in small hashtable: `in` keyword didn't work! check __contains__.\nkey:{}".format(key))
 
-    def testLen(self):
-        q = Hashtable(hashFunction, 1000)
+    def testLargeContains(self):
+        q = Hashtable(hashFunction, 800)
+        for key, value in self.doubles.iteritems():
+            q[key] = value
+        for key in self.doubles:
+            self.assertIn(key, q, "membership in large hashtable: `in` keyword didn't work! check __contains__.\nkey:{}".format(key))
+
+    def testSmallLen(self):
+        q = Hashtable(hashFunction, 3)
         for key, value in self.buildings.iteritems():
             q[key] = value
         l = len(q)
-        self.assertIsInstance(l, type(len(self.buildings)), "length: incorrect type!")
-        self.assertLessEqual(l, len(self.buildings), "length: {} items is too many! expected {}; check __len__.".format(l, len(self.buildings)))
-        self.assertGreaterEqual(l, len(self.buildings), "length: {} items is not enough! expected {}; check __len__.".format(l, len(self.buildings)))
+        self.assertIsInstance(l, type(len(self.buildings)), "length of small hashtable: incorrect type!")
+        self.assertLessEqual(l, len(self.buildings), "length of small hashtable: {} items is too many! expected {}; check __len__.".format(l, len(self.buildings)))
+        self.assertGreaterEqual(l, len(self.buildings), "length of small hashtable: {} items is not enough! expected {}; check __len__.".format(l, len(self.buildings)))
+
+    def testLargeLen(self):
+        q = Hashtable(hashFunction, 800)
+        for key, value in self.doubles.iteritems():
+            q[key] = value
+        l = len(q)
+        self.assertIsInstance(l, type(len(self.doubles)), "length of large hashtable: incorrect type!")
+        self.assertLessEqual(l, len(self.doubles), "length of large hashtable: {} items is too many! expected {}; check __len__.".format(l, len(self.doubles)))
+        self.assertGreaterEqual(l, len(self.doubles), "length of large hashtable: {} items is not enough! expected {}; check __len__.".format(l, len(self.doubles)))
 
 if __name__ == "__main__":
-    unittest.main()
+    # Default to running more verbose tests.
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestHashtable)
+    unittest.TextTestRunner(verbosity=2).run(suite)
