1bfe92d9390c394c9b35f31b40a7aed1e21b5769
Quint Guvernator <quintus.public@gmail.com>
2014-03-28 15:31:03 -0400

    A5: begin writing test cases

---
+++ A5/TestSlidingPuzzle.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python2
+
+from slidingpuzzle import BadMoveException, PuzzleState, PuzzleSolver
+import unittest
+
+class TestPuzzleState(unittest.TestCase):
+
+    def setUp(self):
+        self.eightPuzzle = PuzzleState(
+                dimensions=(3, 3),
+                gamestate=[1, 2, 3, 4, 5, 6, 7, 8, None],
+                parent=None,
+                lastMove=None)
+        self.fifteenPuzzle = PuzzleState(
+                dimensions=(4, 4),
+                gamestate=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, None],
+                parent=None,
+                lastMove=None)
+
+    def testDimensionsInput(self):
+        expected = (3, 3)
+        observed = self.eightPuzzle.dimensions
+        m = "Dimensions weren't what we passed in. Make sure self.dimensions is a tuple of ints.\n\nObserved: {}\nExpected: {}"
+        self.assertEquals(observed, expected, m.format(observed, expected))
+
+    def testItemCount(self):
+        x, y = self.eightPuzzle.dimensions
+        observedList = self.eightPuzzle.gamestate
+        observedCount = len(observedList)
+        observedDimensions = self.eightPuzzle.dimensions
+        expectedCount = x * y
+        m = '''Too many or too few elements in gamestate for current dimensions.
+
+Observed gamestate: {} ({} elements)
+Dimensions: {}'''
+        message = m.format(observedList, observedCount, observedDimensions)
+        self.assertEquals(observedCount, expectedCount, message)
+
+    def testCoordToIndex(self):
+        m = '''Wrong index given from coordinate. Check coordToIndex() method.
+
+Coordinates given: {}
+Observed index: {}
+Expected index: {}'''
+
+        coord = (0, 2)
+        expectedIndex = 6
+        observedIndex = self.eightPuzzle.coordToIndex(coord)
+        message = m.format(coord, observedIndex, expectedIndex)
+        self.assertEquals(observedIndex, expectedIndex, message)
+
+        coord = (0, 0)
+        expectedIndex = 0
+        observedIndex = self.eightPuzzle.coordToIndex(coord)
+        message = m.format(coord, observedIndex, expectedIndex)
+        self.assertEquals(observedIndex, expectedIndex, message)
+
+        coord = (1, 2)
+        expectedIndex = 7
+        observedIndex = self.eightPuzzle.coordToIndex(coord)
+        message = m.format(coord, observedIndex, expectedIndex)
+        self.assertEquals(observedIndex, expectedIndex, message)
+
+        coord = (2, 2)
+        expectedIndex = 8
+        observedIndex = self.eightPuzzle.coordToIndex(coord)
+        message = m.format(coord, observedIndex, expectedIndex)
+        self.assertEquals(observedIndex, expectedIndex, message)
+
+        coord = (2, 0)
+        expectedIndex = 2
+        observedIndex = self.eightPuzzle.coordToIndex(coord)
+        message = m.format(coord, observedIndex, expectedIndex)
+        self.assertEquals(observedIndex, expectedIndex, message)
+
+class TestPuzzleSolver(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def testArbitraryTestcase(self):
+        self.assertEquals(True, True, "message")
+
+
+if __name__ == "__main__":
+    # Load both test suites
+    stateTests = unittest.TestLoader().loadTestsFromTestCase(TestPuzzleState)
+    solverTests = unittest.TestLoader().loadTestsFromTestCase(TestPuzzleSolver)
+
+    # Run both test suites. Default to running more verbose tests.
+    unittest.TextTestRunner(verbosity=2).run(stateTests)
+    unittest.TextTestRunner(verbosity=2).run(solverTests)
